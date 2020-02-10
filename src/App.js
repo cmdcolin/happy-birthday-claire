50fbc7b679d6bb1d2e796c11ecb309d18c0e5528
Colin <colin.diesh@gmail.com>
2020-02-09 22:16:29 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -6,14 +6,23 @@ import truly1 from './truly1.gif'
 import truly2 from './truly2.gif'
 import links from './links.gif'
 import onlytime from './only_time_enya.mid'
+import baby from './dancingbaby.gif'
+import homealone from './homealone.gif'
+import happybirthday from './cooltext.gif'
 
 function App() {
   return (
     <div className="App">
       <div style={{ display: 'flex' }}>
         <img src={truly1} />
-        <div style={{ width: 800 }} />
-        <img src={truly2} />
+        <div style={{ width: 800 }}>
+          <img src={happybirthday} style={{ marginTop: 200 }} />
+          <img src={homealone} style={{ margin: 50 }} />
+        </div>
+        <div style={{ display: 'block' }}>
+          <img src={truly2} style={{ width: 450 }} />
+          <img src={baby} style={{ width: 250 }} />
+        </div>
       </div>
       <a
         href="#"
