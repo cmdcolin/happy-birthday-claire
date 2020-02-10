528e3aa4d6df030bf06144822362f755fd874e26
Colin <colin.diesh@gmail.com>
2020-02-09 21:55:43 -0500

    Updates

---
+++ precache-manifest.5fff21c2b028f5c8ac5a75b3faefc4c0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "148f7b4251ccd0f027afc7763f7a1584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d4bf906b2c2057e9d07",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6768214ea08cdc06c3a4",
+    "url": "/static/js/2.72360023.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.72360023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4bf906b2c2057e9d07",
+    "url": "/static/js/main.fb8847be.chunk.js"
+  },
+  {
+    "revision": "42cefa303bc7fb094144",
+    "url": "/static/js/runtime-main.4453a712.js"
+  },
+  {
+    "revision": "a075928c904a36e4b09db71ea20b9690",
+    "url": "/static/media/links.a075928c.gif"
+  },
+  {
+    "revision": "4798b111c504777a30e59a7ffabcec69",
+    "url": "/static/media/only_time_enya.4798b111.mid"
+  },
+  {
+    "revision": "ffd608fcd51db70e2ad74ce1d06f5492",
+    "url": "/static/media/truly1.ffd608fc.gif"
+  },
+  {
+    "revision": "3e8bf7f3a26ee8fbca410f6759459df5",
+    "url": "/static/media/truly2.3e8bf7f3.gif"
+  }
+]);
